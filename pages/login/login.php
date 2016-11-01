d46b9370b814bf780e34a67ea8e91eac808b2c1e
Dhwani <dhwanikapadia1234@gmail.com>
2016-11-01 17:37:01 +0530

    home added

--- pages/login/login.php
+++ pages/login/login.php
@@ -1,18 +1,24 @@
-﻿<!DOCTYPE html>
+﻿<?php
+	//Start session
+	session_start();	
+	//Unset the variables stored in session
+	unset($_SESSION['SESS_MEMBER_ID']);
+	unset($_SESSION['SESS_FIRST_NAME']);
+	unset($_SESSION['SESS_LAST_NAME']);
+?>
+
+<!DOCTYPE html>
 <html >
   <head>
     <meta charset="UTF-8">
     <title>Sign-Up/Login Form</title>
     <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="../layout/styles/login.css"> 
-    <link rel="stylesheet" href="../layout/styles/login1.css"> 
-    
+    <link rel="stylesheet" href="../../layout/styles/login.css"> 
+    <link rel="stylesheet" href="../../layout/styles/login1.css"> 
   </head>
 
   <body>
-
     <div class="form">
-      
       <ul class="tab-group">
         <li class="tab active"><a href="#signup">Sign Up</a></li>
         <li class="tab"><a href="#login">Log In</a></li>
@@ -23,7 +29,6 @@
           <h1>WELCOME</h1>
           
           <form action="/" method="post">
-          
           <div class="top-row">
             <div class="field-wrap">
               <label>
@@ -64,7 +69,17 @@
           <h1>Welcome Back!</h1>
           
           <form action="/" method="post">
-          
+          	<?php
+						if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
+						echo '<ul class="err">';
+						foreach($_SESSION['ERRMSG_ARR'] as $msg) {
+							echo '<li>',$msg,'</li>'; 
+							}
+						echo '</ul>';
+						unset($_SESSION['ERRMSG_ARR']);
+						}
+					?>
+            
             <div class="field-wrap">
             <label>
               Email Address<span class="req">*</span>
@@ -92,10 +107,6 @@
 </div> <!-- /form -->
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
-        <script src="../layout/scripts/login.js"></script>
-
-    
-    
-    
+        <script src="../../layout/scripts/login.js"></script>
   </body>
 </html>
